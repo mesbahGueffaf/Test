0e9600256c9f4928287b4d289022a67501e88109
mesbahGueffaf <mesbah.s2i@gmail.com>
2019-02-10 13:50:38 +0100

    first java commit

---
+++ helloWord.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author gueffaf.mesbah
+ */
+public class helloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("hello word" ); 	
+    }
+    
+}
